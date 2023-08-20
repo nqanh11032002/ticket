8058d9f183a52f7cb51d5c25ff48de5fa9c20a08
NQA11032002 <kawasakisga@gmail.com>
2023-08-20 20:00:47 +0700

    first code movie service

---
+++ src/test/java/com/guild/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guild.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
