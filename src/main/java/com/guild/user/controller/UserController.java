6f9d23b6d3aa04b471ee725e3ce626bd2488ed2a
nguyen <78311367+ngJay14@users.noreply.github.com>
2023-08-23 10:15:31 +0700

    Update UserController.java

--- src/main/java/com/guild/user/controller/UserController.java
+++ src/main/java/com/guild/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/")
 public class UserController implements IUserController {
 
     @Autowired
