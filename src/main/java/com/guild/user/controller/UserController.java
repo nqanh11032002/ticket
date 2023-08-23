3707a1f75f926c4c7b8743d3dc08911202aa319c
sucuritytech <huunguyenz123@gmail.com>
2023-08-23 08:37:23 +0700

    Update user service

--- src/main/java/com/guild/user/controller/UserController.java
+++ src/main/java/com/guild/user/controller/UserController.java
@@ -19,7 +19,7 @@ public class UserController implements IUserController {
 
     //List all users
     @Override
-    @GetMapping("/listUsers")
+    @GetMapping("")
     @PreAuthorize("hasRole('client_admin')")
     public ResponseObject getAllUser() {
         return iUserService.getAllUser();
