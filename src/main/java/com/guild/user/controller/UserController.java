376e4d5eb8cade132d7700ac2ab782176763d2c5
sucuritytech <huunguyenz123@gmail.com>
2023-08-23 10:32:02 +0700

    Update user service

--- src/main/java/com/guild/user/controller/UserController.java
+++ src/main/java/com/guild/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/user")
 public class UserController implements IUserController {
 
     @Autowired
